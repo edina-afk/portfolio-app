84c56229bb118688d7eb10e2f4e07bb5c4a5211e
edina-afk <edenb928@gmail.com>
2024-09-14 14:34:09 +0300

    Updated About page

---
+++  new (1)/ new/src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,31 @@
+import  "./portfolio.scss"
+
+export default function Portfolio() {
+  return (
+    <div className="portfolio" id="portfolio">
+        <div className="title">
+                 <h1>About Me</h1>
+        </div>
+           <div className="container">
+                <div className="content">
+                  <h2>Welcome to my website</h2>
+                <p>
+                Welcome to my web developer portfolio! I'm Mitchell, a skilled and
+                creative web developer with a passion for creating beautiful,
+                responsive, and user-friendly websites. I've worked on a variety of
+                web projects, ranging from personal blogs to e-commerce platforms.
+            </p>
+              </div>
+            <div className="section">
+             <video controls>
+              <source src="image-4-1_eZquOQZO.mp4" type="video/mp4"/>
+             </video>
+            </div>
+        </div>
+    </div>
+         
+        
+     
+
+  )
+}
